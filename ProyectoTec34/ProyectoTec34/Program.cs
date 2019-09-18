053e0e8ba1cdba249a3c3cda04aca10305b7bfa0
Arturo <arturoequinox@gmail.com>
2019-09-18 04:59:19 -0500

    Asignado index como pantalla principal

--- ProyectoTec34/ProyectoTec34/Program.cs
+++ ProyectoTec34/ProyectoTec34/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoTec34
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home.Index());
         }
     }
 }
